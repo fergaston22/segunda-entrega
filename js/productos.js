0f7db99902cced9d80171e7e2453bfcceb15eac6
fernando espinola <musikk.fercha@gmail.com>
2022-03-23 20:53:37 -0300

    segunda entrega coderhouse java

---
+++ js/productos.js
@@ -0,0 +1,14 @@
+const productos = [
+    {id:01, tipo:"Remera", nombre:"Musculosa Good Times", precio: 1400, foto: "./media/muscu good times.jpg" },
+    {id:02, tipo:"Remera", nombre:"Remera Aeternum", precio: 1400, foto: "./media/reme aeternum.jpg" },
+    {id:05, tipo:"Remera", nombre:"Remera Good Times", precio: 1400, foto: "./media/reme good times.jpg" },
+    {id:07, tipo: "Buzo", nombre: "Buzo Scream", precio: 4500, foto: "./media/buzo scream.jpg"},
+    {id:08, tipo: "Buzo", nombre: "Buzo Negro Oversized", precio: 3800, foto: "./media/buzo negro oversized.jpg"},
+    {id:09, tipo: "Buzo", nombre: "Buzo Batik Oversized", precio: 3800, foto: "./media/buzo batik.jpg"},
+    {id:10, tipo: "Buzo", nombre: "Hoddie Heads", precio: 3800, foto: "./media/hoddie heads.jpg"},
+    {id:14, tipo: "Biker", nombre: "Biker black", precio: 1300, foto: "./media/biker black.jpg"},
+    {id:15, tipo: "Accesorio", nombre: "Lentes Atria", precio: 2300, foto: "./media/lentes atria.jpg"},
+    {id:16, tipo: "Accesorio", nombre: "Lentes Venatori", precio: 2400, foto: "./media/lentes venatori.jpg"},
+    {id:17, tipo: "Accesorio", nombre: "Beanie Pink", precio: 800, foto: "./media/beanie pink.jpg"},
+    {id:18, tipo: "Accesorio", nombre: "Beanie Violet", precio: 800, foto: "./media/beanie violet.jpg"}
+];
